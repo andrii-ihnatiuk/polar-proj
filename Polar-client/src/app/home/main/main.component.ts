1d710cdd51090bc08ce061fa45b7e2f37a2db0ca
Andrew <andreyignatuk2002@gmail.com>
2021-11-05 17:39:29 +0200

    Small improvements

--- Polar-client/src/app/home/main/main.component.ts
+++ Polar-client/src/app/home/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -8,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 export class MainComponent implements OnInit {
   title = 'Polar-client';
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  goTest() {
+    this.router.navigate(['/test'])
   }
 
 }
